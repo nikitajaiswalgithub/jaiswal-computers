4cb0795b0bbf37ffac5059de5a2037abf5a6a63c
Nikita Jaiswal <135646609+nikitajaiswalgithub@users.noreply.github.com>
2024-09-02 00:08:21 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+const open_menu = document.querySelector('.fa-bars')
+const close_menu = document.querySelector('.fa-square-xmark');
+const nav_active = document.querySelectorAll('.navbar');
+
+open_menu.addEventListener('click', () => {
+    nav_active.forEach((elem) => elem.classList.add('active') )
+})
+
+close_menu.addEventListener('click', () => {
+    nav_active.forEach(elem => elem.classList.remove('active'))
+})
+
+
+
+document.getElementById('back-arrow').addEventListener('click', function() {
+    window.history.back();
+});
